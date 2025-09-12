c1eacc3312d3f9f03b95521e7b7df62c588efd1e
Romina <rominaggargano@gmail.com>
2025-09-12 14:24:26 -0300

    landing inicial

---
+++ js/romina-gargano.js
@@ -0,0 +1,27 @@
+// Botón subir
+
+const btnUp = document.getElementById("btnUp");
+
+window.addEventListener("scroll", ()=>{
+    if (window.scrollY > 100){
+        btnUp.style.display = "flex";
+
+    }
+    else{
+        btnUp.style.display = "none";
+    }
+});
+
+btnUp.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const btnDarkMode = document.getElementById("btnDarkMode");
+    btnDarkMode.addEventListener("click", ()=>{
+        document.body.classList.toggle("dark-mode");
+    });
+});
